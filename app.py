522fa78791662c04537a4a9d7b4238f6d45a3c62
suresh anuganti <sureshmecad@gmail.com>
2021-08-18 19:12:57 +0530

    CHANGE03:Added-pickle.file

--- app.py
+++ app.py
@@ -1,21 +1,10 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Dec 16 11:30:53 2020
-
-@author: TRANG THOMAS
-"""
 import numpy as np
 import pickle
 import sklearn
 from flask import Flask,render_template,request,jsonify
 
-
-
-
-
-
 app = Flask(__name__)
-model = pickle.load(open('model.pkl', 'rb'))
+model = pickle.load(open('LGBM.pkl', 'lgbm'))
 
 
 
@@ -33,7 +22,7 @@ def predict():
 
     output = round(prediction[0],2)
 
-    return render_template('index.html', prediction_text='The predicted number of rented bikes is {}'.format(output))
+    return render_template('index.html', prediction_text='The predicted trip duration {}'.format(output))
 
 
 
--- request.py
+++ request.py
@@ -1,15 +1,8 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Dec 28 01:56:13 2020
-
-@author: TRANG THOMAS
-"""
 import requests
 
 url = 'http://localhost:5000/predict_api'
-r = requests.post(url,json={'Hour (0 to 24)':10, 'Temperature (°C)':10, 'Humidity (%)':60,'Wind Speed (m/s)':3,
-                            'Visibility (10m)':1000
-                            ,'Solar radiation (MJ/m2)':0,'Rainfall (mm)':0,'Snowfall (cm)':2,'Month':12
-                            ,'WeekDay (1 to 7)': 1 })
+r = requests.post(url,json={'Distance':10, 'Temperature (°C)':10, 'Humidity (%)':60,'Wind Speed (m/s)':3,
+                            'Haversine':1000, 'Solar radiation (MJ/m2)':0, 'PLong (km)':0, 'DLong (km)':2,
+                            'Pmonth':12,'Phour': 1, 'Dmonth':2, 'Dhour':2})
 
 print(r.json())
